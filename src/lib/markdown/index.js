e653006c2f6fc8b53cb70a2e53016aedf6f70c2c
Andriy Kashcha <anvaka@gmail.com>
2017-03-26 20:43:37 -0700

    Added support for custom image sizes

--- src/lib/markdown/index.js
+++ src/lib/markdown/index.js
@@ -16,15 +16,41 @@ function createCustomRenderer() {
   const renderer = new marked.Renderer();
   const originalLink = renderer.link;
 
-  renderer.link = function link(href, title, text) {
+  renderer.link = link;
+  renderer.image = image;
+
+  return renderer;
+
+  function link(href, title, text) {
     const youtubeVideoId = isYouTubueLink(href);
     if (youtubeVideoId) {
       return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeVideoId}" frameborder="0" allowfullscreen></iframe>`;
     }
     return originalLink.call(renderer, href, title, text);
-  };
+  }
 
-  return renderer;
+  function image(href, title, text) {
+    const imageSizeMatch = /([\s\S]+) =([0-9]*)x([0-9]*)/.exec(href);
+
+    if (imageSizeMatch) href = imageSizeMatch[1];
+
+    let out = '<img src="' + href + '" alt="' + text + '"';
+    if (title) {
+      out += ' title="' + title + '"';
+    }
+
+    if (imageSizeMatch) {
+      if (imageSizeMatch[2]) {
+        out += ' width="' + Number.parseInt(imageSizeMatch[2], 10) + '"';
+      }
+      if (imageSizeMatch[3]) {
+        out += ' height="' + Number.parseInt(imageSizeMatch[3], 10) + '"';
+      }
+    }
+
+    out += this.options.xhtml ? '/>' : '>';
+    return out;
+  }
 }
 
 function isYouTubueLink(href) {
