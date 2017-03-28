8c531657d20926266ed6d23efc11944cb3d78c3d
Andriy Kashcha <anvaka@gmail.com>
2017-03-27 21:59:05 -0700

    Trying to fix token's autorefresh
    Google has changed the error message, and it started failing. Not sure
    if the one I have now is stable enough

--- src/lib/gapi/handleAuthError.js
+++ src/lib/gapi/handleAuthError.js
@@ -2,8 +2,10 @@ export default handleAuthError;
 
 function handleAuthError(retryCallback, rejectCallback) {
   return err => {
+    // TOOD: should I give up at some point?
     const isInvalidCredentials = (err.status === 401) &&
-      err.result && (err.result.error.message === 'Invalid Credentials');
+      err.result && (err.result.error.status === 'UNAUTHENTICATED');
+    // err.result.error.message === 'Invalid Credentials');
 
     if (isInvalidCredentials) {
       return (
