637865ec726383a2a7fb10381ab9bba7d12bafea
Andriy Kashcha <anvaka@gmail.com>
2017-03-29 21:53:40 -0700

    Discovered one more case of token expiration

--- src/lib/gapi/handleAuthError.js
+++ src/lib/gapi/handleAuthError.js
@@ -4,8 +4,12 @@ function handleAuthError(retryCallback, rejectCallback) {
   return err => {
     // TOOD: should I give up at some point?
     const isInvalidCredentials = (err.status === 401) &&
-      err.result && (err.result.error.status === 'UNAUTHENTICATED');
-    // err.result.error.message === 'Invalid Credentials');
+      err.result && (
+        // Google drive returns one error message
+        (err.result.error.status === 'UNAUTHENTICATED') ||
+        // while google sheets another
+        (err.result.error.message === 'Invalid Credentials')
+      );
 
     if (isInvalidCredentials) {
       return (
