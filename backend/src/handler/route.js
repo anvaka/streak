16d5001eb65bf7bc33bb1613e3596e0770785bc9
Andriy Kashcha <anvaka@gmail.com>
2017-05-20 21:56:30 -0700

    Added datastore for the backend

--- backend/src/handler/route.js
+++ backend/src/handler/route.js
@@ -1,6 +1,9 @@
 const saveProjectPublic = require('./operations/saveProjectPublic.js');
 const listProjects = require('./operations/listProjects.js');
 
+const gapiListProjects = require('./operations/gapi/listProjects.js');
+const gapiUpdateProject = require('./operations/gapi/listProjects.js');
+
 module.exports = route;
 
 function route(req) {
@@ -10,8 +13,14 @@ function route(req) {
   console.log('routing ', operation);
   switch (operation) {
     case 'set-project-public':
+      gapiUpdateProject(req.body, req.user).then(res => {
+        console.log('Gapi update project: ', JSON.stringify(res));
+      });
       return saveProjectPublic(req);
     case 'list-projects':
+      gapiListProjects(queryString).then(res => {
+        console.log('Gapi list projects: ', JSON.stringify(res));
+      });
       return listProjects(queryString);
     default:
       throw new Error('Not implemented');
